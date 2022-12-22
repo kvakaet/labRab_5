1b95b1feafe622865efea487e8d8e2873ca9b1ec
Chmonya <towerland2801@gmail.com>
2022-12-22 06:57:40 +0300

    fix

--- task/Hard_task.py
+++ task/Hard_task.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import math
+import sys
+
+EPS = 1e-10
 
 # вариант 5
 if __name__ == '__main__':
-    x = int(input("введите x: "))
-    row = 0
-    if x >= 0:
-        for n in range(x + 1):
-            row += (pow(-1, n) * pow(math.pi / 2, 2 * n)) / (math.factorial(2 * n) * (4 * n - 1))
-        print(format(row, ".11"))
-    else:
-        print("ошибка")
+    x = float(input("введите x: "))
+    if x == 0:
+        print("неверно введенное x", file=sys.stderr)
+        exit(1)
+
+    a = -((math.pi ** 2) / 40)
+    S, n = a, 1
+
+    while math.fabs(a) < EPS:
+
+        a *= -((math.pi ** 2) * (4 * n - 1)) / 4 * (2 * n + 2) * (2 * n + 1) * (4 * n + 5)
+        S += a
+        n += 1
+
+    print(S)
